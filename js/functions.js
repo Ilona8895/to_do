d1b98f22c31e36560edcf9632decd3a83945c120
Ilona Melcher <ilona.melcher8@gmail.com>
2024-11-08 10:05:03 +0100

    add url

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,8 @@
-// const apiUrls = {
-//   production: "https://to-do-nodejs-jih7.onrender.com",
-//   development: "http://127.0.0.1:8888",
-// };
+const productionURL = "https://to-do-nodejs-jih7.onrender.com";
+const developmentURL = "http://127.0.0.1:8888";
 
 function sendItemToBackend(item) {
-  fetch("https://to-do-nodejs-jih7.onrender.com/todo", {
+  fetch(`${productionURL}/todo`, {
     method: "POST",
     body: JSON.stringify(item),
     headers: {
@@ -14,19 +12,17 @@ function sendItemToBackend(item) {
 }
 
 function getItemsFromBackend() {
-  return fetch("https://to-do-nodejs-jih7.onrender.com/todo").then((res) =>
-    res.json()
-  );
+  return fetch(`${productionURL}/todo`).then((res) => res.json());
 }
 
 function removeItemFromBackend(item) {
-  fetch(`https://to-do-nodejs-jih7.onrender.com/todo/${item}`, {
+  fetch(`${productionURL}/todo/${item}`, {
     method: "DELETE",
   });
 }
 
 function patchItemToBackend(update) {
-  fetch(`https://to-do-nodejs-jih7.onrender.com/todo/${update.id}`, {
+  fetch(`${productionURL}/todo/${update.id}`, {
     method: "PATCH",
     body: JSON.stringify(update),
     headers: {
